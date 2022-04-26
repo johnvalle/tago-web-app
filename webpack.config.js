421d5c1af0ed5a8737e8ba2c358af2453ac203dc
John Valle <jvalle@Johns-MacBook-Air.local>
2022-04-26 14:36:04 +0800

    Added inital webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  outputh: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};

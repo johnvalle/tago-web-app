6ad3c9001271c0c14e4a051ae21e42175f83d729
johnvalle <vallejohn.personal@gmail.com>
2023-11-20 17:20:07 +0800

    Add fake tables

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,15 +1,15 @@
-import { Dexie } from 'dexie';
+import { Dexie, DexieConstructor, DexieOptions } from 'dexie';
+import indexedDB from 'fake-indexeddb'
 import { ICategory } from './models/Category/types';
 
 import { ISecuredItem } from './models/SecuredItem/types';
 
 export class TagoDatabase extends Dexie {
   public securedItems!: Dexie.Table<ISecuredItem, 'id'>;
-
   public category!: Dexie.Table<ICategory, 'id'>;
 
-  constructor() {
-    super('TagoDatabase');
+  constructor(databaseName: string, options?: DexieOptions) {
+    super(databaseName, options);
     this.version(1).stores({
       securedItems: '++id, name, encryptedValue, dateCreated, isEncrypted, category',
       category: '++id, name, dateCreated',
@@ -17,4 +17,5 @@ export class TagoDatabase extends Dexie {
   }
 }
 
-export const AppDB = new TagoDatabase();
+export const FakeDB = new TagoDatabase("FakeDatabase", { indexedDB: indexedDB });
+export const AppDB = new TagoDatabase("TagoDatabase");
